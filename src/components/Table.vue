40871b54516f1e5819166b512737dd127630cba3
Victor <victor.archambault@telecom-st-etienne.fr>
2020-11-27 13:03:18 +0100

    Added Date and Time to the table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -20,6 +20,24 @@
           class="form-control"
         />
       </div>
+      <div class="form-group">
+        <label for="date">Date</label>
+        <input 
+          type="date" 
+          v-model="date"
+          class="form-control"
+        />
+      </div>
+      <div>
+        <!-- TODO Find a way to add placeholder time-->  
+        <label for="time">Time</label>
+        <input
+          type="time"
+          v-model="time"
+          class="form-control"
+          step="1"
+        />
+      </div>
       <button type="button" @click="onSubmit" class="btn btn-dark">
         Submit
       </button>
@@ -30,6 +48,8 @@
           <th scope="col">#</th>
           <th scope="col">Timestamp</th>
           <th scope="col">Value</th>
+          <th scope="col">Date</th>
+          <th scope="col">Time</th>
         </tr>
       </thead>
       <tbody>
@@ -37,6 +57,8 @@
           <th scope="row">{{ ++i }}</th>
           <td>{{ entry.name }}</td>
           <td>{{ entry.score }}</td>
+          <td>{{ entry.date }}</td>
+          <td>{{ entry.time }}</td>
         </tr>
       </tbody>
     </table>
@@ -46,22 +68,36 @@
 <script>
 export default {
   name: "Table",
-  data: () => ({ name: "", score: "", allScores: [] }),
+  data: () => ({
+    name: "",
+    score: "",
+    allScores: [],
+    date: new Date().toISOString().substr(0, 10),
+    time: ""
+  }),
   computed: {
-    sortedList: function() {
-      return this.allScores.slice().sort(function(a, b) {
+    sortedList: function () {
+      return this.allScores.slice().sort(function (a, b) {
         return b.score - a.score;
       });
     },
   },
   methods: {
     onSubmit() {
-      this.allScores.push({ name: this.name, score: this.score });
+      this.allScores.push({
+        name: this.name,
+        score: this.score,
+        date: this.date,
+        time: this.time
+      });
       this.clearForm();
     },
     clearForm() {
       this.name = "";
       this.score = "";
+      var t = new Date();
+      this.date = t.getDate;
+      this.time = t.getTime();
     },
   },
 };
