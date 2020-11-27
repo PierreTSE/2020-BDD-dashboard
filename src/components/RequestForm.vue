90a29b3cf38704405b572f197a0e16e89de82918
Julien Giovinazzo <juliengiovinazzo@gmail.com>
2020-11-27 15:37:45 +0100

    Bug fix

--- src/components/RequestForm.vue
+++ src/components/RequestForm.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container">
         <form class="form-inline">
-            <input class="form-control" type="text" placeholder="Timestamp">
-            <button class="btn btn-info" @click="onRequestSubmit">Request</button>
+            <input class="form-control" v-model="timestamp" type="text" placeholder="Timestamp">
+            <button type="button" class="btn btn-info" @click="onRequestSubmit()">Request</button>
         </form>
     </div>
 </template>
@@ -11,9 +11,15 @@
 <script>
 export default {
     name: "RequestForm",
+    data() {
+        return {
+            timestamp: "",
+        }
+    },
     methods: {
         async onRequestSubmit() {
-            let queryStr = "SELECT ";
+            let queryStr = "SELECT " + this.timestamp;
+            console.log(queryStr);
             try {
                 let response = await fetch("http://localhost:8080/query?query=" + queryStr);
                 const data = await response.json();
@@ -23,7 +29,7 @@ export default {
                     throw new Error("Soucis de reponse. Code reçu : " + data.code);
                 }
             } catch (err) {
-                console.error("Huston, nous avons un problème! ", err);
+                console.error("Huston, nous avons un problème! (surement que nous n'avons pas reçu de réponse du serveur).", err);
             }
             
             this.clearForm();
