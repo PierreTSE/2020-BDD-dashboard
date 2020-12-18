524aa41e70c5e327f9cc80a660bb24a395d7d178
ComteHerrapait <leon.delcroix@telecom-st-etienne.fr>
2020-12-18 14:28:27 +0100

    exact date and bugfixes

--- src/components/RequestForm.vue
+++ src/components/RequestForm.vue
@@ -15,10 +15,9 @@
         <form class="form-group form-inline">
             <input type="date" class="form-control flex-fill" v-model="date_before" v-if="date_before_enable" @change="updateRequest()"/>
             <input type="date" class="form-control flex-fill" v-model="date_after" v-if="date_after_enable" @change="updateRequest()"/>
-            <input type="date" class="form-control flex-fill" v-model="date_exact" v-if="request_mode_all" @change="updateRequest()"/>
-            <input type="text" class="form-control flex-fill" v-model="manual_query" v-if="manual_query_enable" @keyup="updateRequest()"/>
-            <button type="button" class="btn btn-info" @click="onRequestAllSubmit()" v-if="request_mode_all">Requete (TOUT)</button>
-            <button type="button" class="btn btn-info" @click="onRequestSubmit()" v-else>Requete</button>
+            <input type="date" class="form-control flex-fill" v-model="date_exact" v-if="request_mode_all" @change="updateCheckboxes()"/>
+            <input type="text" class="form-control flex-fill" v-model="manual_query" v-if="manual_query_enable" @keyup="updateRequest()" @keydown="updateRequest()"/>            
+            <button type="button" class="btn btn-info" @click="onRequestSubmit()">{{submit_text}}</button>
         </form>
     </div>
 </template>
@@ -28,6 +27,7 @@
 export default {
     name: "RequestForm",
     data: () => ({   
+        submit_text: null,
         date_before_enable: false,
         date_after_enable: false,
         manual_query_enable: false,
@@ -42,38 +42,45 @@ export default {
     }),
     methods: {
         updateRequest(){
-            let prefix = "SELECT all FROM";
-            prefix += " " + this.serie;
+            //TODO : change function organisation to avoid all `return;` (not pretty)
 
+            //initialization
+            let prefix = "SELECT FROM " + this.serie;
+            let conditions = []
+            let conditions_processed = "";
+
+            //load conditions for the request
             if (this.request_mode_all) {
-                this.request =  prefix
+                if (this.date_exact) {
+                    conditions.push("timestamp == " + Date.parse(this.date_before)/1000);
+                } else {
+                    this.request =  "SELECT all FROM " + this.serie;
+                    return; //escape to avoid overwrite later in function
+                }
             } 
             else if (this.manual_query_enable){
                 this.request = this.manual_query;
+                return; //escape to avoid overwrite later in function
             } 
             else {
-                let conditions = []
                 // TODO : Do we want finer controls ? like choose with hour rather than date (pls no)
                 if (this.date_before_enable){
-                    conditions.push("timestamp < " + Date.parse(this.date_before)/1000);
+                    conditions.push("timestamp <= " + Date.parse(this.date_before)/1000);
                 }
                 if (this.date_after_enable){
-                    conditions.push("timestamp > " + Date.parse(this.date_after)/1000);
+                    conditions.push("timestamp >= " + Date.parse(this.date_after)/1000);
                 }
-
-                let conditions_processed;
-                if (conditions.length == 1) {
-                    conditions_processed = "WHERE "+conditions[0];
-                } else {
-                    conditions_processed = "WHERE "
-                    conditions.forEach(cond => {
-                        conditions_processed += cond + " AND "
-                    });
-                    conditions_processed = conditions_processed.slice(0, -5)
-                }
-                this.request = prefix + conditions_processed + ";";
-                // TODO : handle case where a specific date is wanted
             }
+
+            //apply conditions to request if needed 
+            if (conditions.length > 0) {
+                conditions_processed = "WHERE "
+                conditions.forEach(cond => {
+                    conditions_processed += cond + " AND "
+                });
+                conditions_processed = conditions_processed.slice(0, -5)
+            }
+            this.request = prefix + conditions_processed + ";";
         },
 
         onRequestSubmit() {
@@ -102,6 +109,8 @@ export default {
             this.value_max = null;
             this.date_before = new Date().toISOString().substr(0, 10);
             this.date_after = new Date().toISOString().substr(0, 10);
+            this.date_exact = new Date().toISOString().substr(0, 10);
+            this.manual_query = null;
         },
 
         updateCheckboxes(manual_query_mode=false) {
@@ -112,9 +121,17 @@ export default {
                 this.manual_query_enable = false;
             }
             this.request_mode_all = !(this.manual_query_enable | this.date_before_enable | this.date_after_enable);
+            if (this.request_mode_all & !this.date_exact) {
+                this.submit_text = "Requete (Tout)"
+            } else {
+                this.submit_text = "Requete"
+            }
             this.updateRequest()
         }
-    }
+    },
+    created(){  //execute on page load
+        this.updateCheckboxes();
+    },
 };
 </script>
 
