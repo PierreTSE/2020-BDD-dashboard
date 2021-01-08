30d2bdad9a31cfbda6213f5ead627f9dea16180a
Reizorc <reizorc77@gmail.com>
2021-01-08 15:54:35 +0100

    Menu collapse

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,38 +1,18 @@
 <template lang="html">
-
   <header class="">
     <div>
-   <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Dashboard</b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-         <b-navbar-nav>
-            <b-nav-item href="#">About</b-nav-item>
-            <b-nav-item href="#" disabled>Contacts</b-nav-item>
-         </b-navbar-nav>
-         <!-- Right aligned nav items -->
-         <b-navbar-nav class="ml-auto">
-            <b-nav-item-dropdown text="Lang" right>
-               <b-dropdown-item href="#">EN</b-dropdown-item>
-               <b-dropdown-item href="#">ES</b-dropdown-item>
-               <b-dropdown-item href="#">RU</b-dropdown-item>
-               <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
-            <b-nav-item-dropdown right>
-               <!-- Using 'button-content' slot -->
-               <template v-slot:button-content>
-                  <em>User</em>
-               </template>
-               <b-dropdown-item href="#">Profile</b-dropdown-item>
-               <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-         </b-navbar-nav>
-      </b-collapse>
-   </b-navbar>
-  
-</div>
+      <b-navbar toggleable="xl" type="dark" variant="info">
+        <b-navbar-brand href="#">Dashboard</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" class="menu-test" style="width:100%">
+          <b-navbar-nav>
+            <b-nav-item href="#">Serie1</b-nav-item>
+            <b-nav-item href="#">Serie2</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
   </header>
-
 </template>
 
 <script lang="js">
@@ -55,13 +35,11 @@
 
     }
 }
-
-
 </script>
 
 <style>
 header {
-  height: 70px;  /* Meme valeur que 'top' de MySidebar */
+  height: 70px; /* Meme valeur que 'top' de MySidebar */
   position: fixed;
   top: 0;
   left: 0;
@@ -70,6 +48,9 @@ header {
   padding: 5px;
   border-bottom: 3px solid black;
 }
-
-
+@media only screen and (min-width: 1200px) {
+  .menu-test {
+    display: none;
+  }
+}
 </style>
