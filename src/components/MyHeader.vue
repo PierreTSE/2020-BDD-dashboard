98875ca6042b047bd31bb0ba56d625b4e1d67988
bubunzer <sobunung@yahoo.fr>
2021-01-08 15:36:43 +0100

    working dropdown

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,21 +1,36 @@
 <template lang="html">
 
   <header class="">
-    <div class="container-fluid pos-f-t">
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Dashboard</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        </nav>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item"><a class="nav-link" href="#">Serie1</a></li>
-            <li class="nav-item"><a class="nav-link " href="#">Serie2</a></li>
-            <li class="nav-item"><a class="nav-link " href="#">Serie3</a></li>
-          </ul>
-        </div>
-    </div>
+    <div>
+   <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Dashboard</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+         <b-navbar-nav>
+            <b-nav-item href="#">About</b-nav-item>
+            <b-nav-item href="#" disabled>Contacts</b-nav-item>
+         </b-navbar-nav>
+         <!-- Right aligned nav items -->
+         <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown text="Lang" right>
+               <b-dropdown-item href="#">EN</b-dropdown-item>
+               <b-dropdown-item href="#">ES</b-dropdown-item>
+               <b-dropdown-item href="#">RU</b-dropdown-item>
+               <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item-dropdown right>
+               <!-- Using 'button-content' slot -->
+               <template v-slot:button-content>
+                  <em>User</em>
+               </template>
+               <b-dropdown-item href="#">Profile</b-dropdown-item>
+               <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+         </b-navbar-nav>
+      </b-collapse>
+   </b-navbar>
+  
+</div>
   </header>
 
 </template>
