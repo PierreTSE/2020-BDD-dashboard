53987374512b3603d43222029a0727424646f36e
Victor <victor.archambault@telecom-st-etienne.fr>
2021-01-08 15:01:05 +0100

    Added base for graph using vue-trend-chart

--- src/components/MyGraph.vue
+++ src/components/MyGraph.vue
@@ -1,12 +1,33 @@
 <template lang="html">
 
   <div id="MyGraph" class="my-4"> 
-    Graphique TO DO
+    <TrendChart
+      :datasets="[
+        {
+          data: [10, 50, 20, 100, 40, 60, 80],
+          smooth: true,
+          fill: true
+        }
+      ]"
+      :grid="{
+        verticalLines: true,
+        horizontalLines: true
+      }"
+      :labels="{
+        xLabels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        yLabels: 5,
+      }"
+      :min="0">
+    </TrendChart>
   </div>
 
 </template>
 
 <script lang="js">
+  import Vue from "vue";
+  import TrendChart from "vue-trend-chart";
+
+  Vue.use(TrendChart);
 
   export default  {
     name: 'MyGraph',
@@ -32,7 +53,6 @@
 
 <style scoped>
   #MyGraph {
-    background-color: lightseagreen;
     width: 100%;
     height: 250px;
   }
