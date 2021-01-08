f10c5de8b47f981dd362c76dcececc5ffd5671ce
ComteHerrapait <leon.delcroix@telecom-st-etienne.fr>
2021-01-08 15:34:20 +0100

    add values to graph

--- src/components/MyGraph.vue
+++ src/components/MyGraph.vue
@@ -1,24 +1,25 @@
 <template lang="html">
-
   <div id="MyGraph" class="my-4"> 
     <TrendChart
       :datasets="[
         {
-          data: [10, 50, 20, 100, 40, 60, 80],
+          data: y_values,
           smooth: true,
-          fill: true
+          fill: true,
+          showPoints: true,
         }
       ]"
       :grid="{
-        verticalLines: true,
+        verticalLines: false,
         horizontalLines: true
       }"
       :labels="{
-        xLabels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        xLabels: x_values,
         yLabels: 5,
       }"
       :min="0">
     </TrendChart>
+    <button @click="addValue()">DEBUG(add value)</button>
   </div>
 
 </template>
@@ -26,7 +27,6 @@
 <script lang="js">
   import Vue from "vue";
   import TrendChart from "vue-trend-chart";
-
   Vue.use(TrendChart);
 
   export default  {
@@ -35,25 +35,30 @@
     mounted () {
 
     },
-    data () {
-      return {
-
-      }
-    },
+    data: () => ({
+      x_values: [0,1,2],
+      y_values: [0,2,1],
+    }),
     methods: {
-
+      addValue(){
+        //TODO: get values from re
+        this.x_values.push(Math.floor(Math.random() * 5));
+        this.y_values.push(Math.floor(Math.random() * 5));
+      },
+      updateGraphValues(x_,y_){
+        this.x_values = x_;
+        this.y_values = y_;
+      }
     },
     computed: {
 
     }
-}
-
-
+  }
 </script>
 
 <style scoped>
-  #MyGraph {
-    width: 100%;
-    height: 250px;
-  }
+#MyGraph {
+  width: 100%;
+  height: 250px;
+}
 </style>
