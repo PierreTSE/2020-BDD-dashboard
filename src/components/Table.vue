d172aedbe3a77ce62e801c9cfc9c02398c275342
PierreTSE <pierre--giraud@orange.fr>
2021-01-18 01:05:35 +0100

    passage en deploy, traduction du file-input

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -30,13 +30,13 @@
 
     <p v-if="error != ''" class="h6 text-light p-2 bg-danger mt-1 mb-3">{{error}}</p>
 
-    <div v-if="showFileDiv || showInsertDiv" class="conatiner-fluid bg-info p-2 mt-2 mb-3 mx-3">
+    <div v-if="showFileDiv || showInsertDiv" class="bg-info p-2 mt-2 mb-3 mx-3">
       <div v-if="showFileDiv">
         <p class="text-light h5 pb-1 border-bottom">Importer un fichier CSV</p>
         
         <div class="custom-file">
-          <input type="file" class="custom-file-input" accept=".csv" @change="loadTextFromFile"/><br>
-          <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+          <input type="file" id="file-input" class="custom-file-input" accept=".csv" @change="loadTextFromFile"/><br>
+          <label class="custom-file-label" for="file-input" data-browse="Choisir">Choisissez un fichier CSV</label>
         </div>
         
         <button v-if="!isCsvParsed" type="button" class="btn btn-dark mt-2" disabled>
